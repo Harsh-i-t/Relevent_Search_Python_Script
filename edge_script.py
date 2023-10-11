98a9d1d45cde19a13e2468c60fcf096097e5d122
Harsh-i-t <130602932+Harsh-i-t@users.noreply.github.com>
2023-10-11 20:35:03 +0530

    Update edge_script.py
     - Adding Date column 
    - Linking the result with Excel and MongoDB
    - Gave a separated ID to each search result
    - Mentioned URL in the database
    - Converted webpage into pdf file
    - Counted Number of pages
    - Scrapped And counted the occurance of a keyword in pdf
    - Rated each result quality on the basis of keyword occcurance

--- edge_script.py
+++ edge_script.py
@@ -1,19 +1,20 @@
 # Import the required modules
-from time import sleep
-import requests
-import webbrowser
+import datetime, re, pdfkit
+
+import requests, webbrowser
 import pandas as pd
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
-import pdfkit
+
+
 
 # Take the user input as the search query
 query = input("Enter your search query: ")
 print("Sit Back & Relax , Searching and Redirecting.....")
-sleep(2)
+
 
 # Define the edge browser path
-edge_path = r"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe"
+edge_path = r"C:\Program Files\Edge\Application\edge.exe"
 webbrowser.register('edge', None, webbrowser.BackgroundBrowser(edge_path))
 
 # Use the requests module to get the Google search results page
@@ -22,56 +23,31 @@ response = requests.get("https://www.google.com/search?q=" + query)
 # Use the BeautifulSoup module to parse the HTML content of the response
 soup = BeautifulSoup(response.text, "html.parser")
 
-# Find all the elements that contain the links to the webpages
-links = soup.find_all("a", href=lambda href: href and "/url?q=" in href)
 
-# Create an empty list to store the titles and urls of the webpages
-results = []
 
-# Loop through the first five links
-for link in links[:5]:
-    # Get the url of the webpage by removing the prefix and suffix
-    url = link["href"].replace("/url?q=", "").split("&")[0]
-    
-    # Get the title of the webpage by getting the text content of the link element
-    title = link.text
-    
-    # Append a dictionary with the title and url to the results list
-    results.append({"title": title, "url": url})
-    
-    # Open the webpage in a new tab using the webbrowser module
-    webbrowser.get('edge').open_new_tab(url)
-    
-# Print the results list
-# print(results)
 
-# Create a pandas dataframe from the results list
-df = pd.DataFrame(results)
 
-# Save the dataframe as an excel file using the pandas module
-df.to_excel(f"{query}.xlsx", index=False)
+# Connect to a MongoDB database using the pymongo module and 
+def Connect_MongoDB_and_Excel(results):
 
-# Connect to a MongoDB database using the pymongo module
-client = MongoClient("mongodb://localhost:27017/")
-db = client["google_search"]
-collection = db["results"]
+    # Create a pandas dataframe from the results list
+    df = pd.DataFrame(results)
 
-# Insert the results list as documents into the MongoDB collection
-collection.insert_many(results)
+    # Save the dataframe as an excel file using the pandas module
+    df.to_excel(f"{query}.xlsx",index=False)
 
-print("MongoDB Database Updated : Success")
-print("Excel Sheet Updated : Success")
 
-# Get the url of the first webpage from the results list
-def get_the_working_url(results):
-    for i in range(len(results)):
-            response = requests.get(results[i]["url"])
-            # If the response status code is 200, it means the url is working
-            if response.status_code == 200:
-                # Print the url and break the loop
-                return results[i]["url"] 
+    print("Excel Sheet Created : Success")
 
-first_url = get_the_working_url(results)
+
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["google_search"]
+    collection = db["results"]
+
+    # Insert the results list as documents into the MongoDB collection
+    collection.insert_many(results)
+
+    print("MongoDB Database Updated : Success")
 
 
 def webpage_to_pdf(url):
@@ -85,5 +61,67 @@ def webpage_to_pdf(url):
     #Convert Webpage to PDF
     pdfkit.from_url(url, output_path=f'{query}.pdf', configuration=config)
 
-webpage_to_pdf(first_url)
-print("Webpage copied to PDF : Success")
+def total_Occurance(file_path):
+
+    global num_pages,count
+
+    from pypdf import PdfReader
+
+    reader = PdfReader(file_path)
+    num_pages = len(reader.pages)
+    text = ''
+    for i in range(num_pages):
+        text += reader.pages[i].extract_text()
+
+    word = query
+
+    # Finding and counting the word using a regular expression
+    pattern = r"\b" + word + r"\b" # \b matches word boundaries
+    matches = re.findall(pattern, text, re.IGNORECASE) # re.IGNORECASE ignores case sensitivity
+    count = len(matches)
+
+
+
+
+# Find all the elements that contain the links to the webpages
+links = soup.find_all("a", href=lambda href: href and "/url?q=" in href)
+
+# Create an empty list to store the titles and urls of the webpages
+results = []
+id1 = 1
+# Loop through the first five links
+for link in (links[:10]):
+
+    # Get the url of the webpage by removing the prefix and suffix
+    url = link["href"].replace("/url?q=", "").split("&")[0]
+
+    # Get the title of the webpage by getting the text content of the link element
+    title = link.text
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+
+            # Converting valid url to pdf
+            webpage_to_pdf(url)
+
+            # Checking keyword frequency
+            total_Occurance(f'{query}.pdf')
+
+
+            # Append a dictionary with the title and url to the results list
+            if count <= 2 * num_pages: quality = "Low"
+            elif 2 * num_pages < count <= 4 * num_pages: quality = "Medium"
+            elif count > 4 * num_pages: quality = "High"
+
+            date = datetime.date.today()
+            results.append({"_id":id1,"Date":str(date),"Keyword Searched":query,"Title": title, "Url": url, "Number of Pages":num_pages, "Quality": quality})
+            id1 += 1
+            # Open the webpage in a new tab using the webbrowser module
+            # webbrowser.get('edge').open_new_tab(url)
+    except: continue
+# Print the results list
+# print(results)
+
+
+Connect_MongoDB_and_Excel(results)
+print("Completed Successfull")
